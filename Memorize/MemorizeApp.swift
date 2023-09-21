fdae51403826a5f2f4d283eb8d61da458df48367
Praveen Emani <praveenkumar199324@gmail.com>
2023-09-21 07:54:33 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by praveen emani on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
